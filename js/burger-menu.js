fbccbb719cbc80573b21694972ebcb2b8771c7d7
Erlan Ntzousoupov <->
2025-03-02 00:46:58 +0200

    Add navbar and make burger menu function

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,3 +1,14 @@
 const burger_menu = ()=>{
-    
+    const menu = document.getElementById('nav-bar');
+
+    if(menu.style.visibility === 'hidden'){
+        menu.style.visibility = 'visible';
+        document.getElementById('nav-bar-constructor').style.visibility = 'visible';
+        document.getElementsByClassName('white-scene')[0].style.visibility = 'visible';
+    }
+    else{
+        menu.style.visibility = 'hidden';
+        document.getElementById('nav-bar-constructor').style.visibility = 'hidden';
+        document.getElementsByClassName('white-scene')[0].style.visibility = 'hidden';
+    }
 } 
